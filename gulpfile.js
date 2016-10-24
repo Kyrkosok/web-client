f2c02fa84227e778cce5a25afb240cb81d094334
Albin Larsson <albin.post@gmail.com>
2016-10-24 10:40:46 +0200

    minor fixes to build system

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,23 @@ var gulpIf = require('gulp-if');
 var uglify = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
 var useref = require('gulp-useref');
+var del = require('del');
 
-gulp.task('default', function() {
-  return gulp.src('src/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', autoprefixer()))
-    .pipe(gulpIf('*.css', cleanCSS()))
+gulp.task('default', ['build', 'cleanup']);
+
+gulp.task('build', function() {
+  return gulp.src('./src/**/*')
+    .pipe(gulpIf('*.html', useref()))
+    .pipe(gulpIf('/js/*.js', uglify()))
+    .pipe(gulpIf('/css/*.css', autoprefixer()))
+    .pipe(gulpIf('/css/*.css', cleanCSS()))
     .pipe(gulp.dest('dist'))
+});
+
+gulp.task('cleanup', ['build'], function() {
+  return del([
+    'dist/js/application.js',
+    'dist/js/jquery-3.1.1.js',
+    'dist/css/app.css'
+    ]);
 });
