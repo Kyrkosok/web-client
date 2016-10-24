c738ede0aa012609b71079bc16b000dfc78364cf
Albin Larsson <albin.post@gmail.com>
2016-10-24 10:14:14 +0200

    build system

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,12 @@ var gulpIf = require('gulp-if');
 var uglify = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
 var useref = require('gulp-useref');
-var unCSS = require('gulp-uncss');
 
+gulp.task('default', function() {
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', autoprefixer()))
+    .pipe(gulpIf('*.css', cleanCSS()))
+    .pipe(gulp.dest('dist'))
+});
